66986a5014726597f4ccc539c2ae3a756afadfb8
Avinash <avi.u95@gmail.com>
2020-02-27 23:33:48 +0530

    Implemented Progress Bar Module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RectCardComponent } from './components/rect-card/rect-card.component';
 import { SectionComponent } from './components/section/section.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 
 @NgModule({
-  declarations: [RectCardComponent, SectionComponent],
-  exports: [RectCardComponent, SectionComponent],
+  declarations: [RectCardComponent, SectionComponent, ProgressBarComponent],
+  exports: [RectCardComponent, SectionComponent, ProgressBarComponent],
   imports: [
     CommonModule
   ]
